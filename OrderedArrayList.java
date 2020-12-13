f7fbe116d4d8c16319b433f6c86907b229a1fe55
Reshmi-Anwar <ranwar10@stuy.edu>
2020-12-12 22:29:59 -0500

    add method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,4 +8,14 @@ public class OrderedArrayList <T extends Comparable<T>> extends NoNullArrayList<
   public void add(int index, T element){
     add(element);
   }
+  public boolean add(T element){
+    for(int i = 0; i < size(); i++){
+      if(element.compareTo(get(i)) < 0){
+        super.add(i, element);
+        return true;
+      }
+    }
+    super.add(element);
+    return true;
+  }
 }
