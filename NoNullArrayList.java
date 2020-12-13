6a16d2c67381c422a6eb9dbf23a3abff420b5e35
Reshmi-Anwar <ranwar10@stuy.edu>
2020-12-12 22:15:13 -0500

    set and add methods override

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -3,6 +3,24 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(){
     super();
   }
+  public T set(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException("No nulls allowed");
+    }
+    return (super.set(index, element));
+  }
+  public boolean add (T element){
+    if (element == null){
+      throw new IllegalArgumentException("No nulls allowed");
+    }
+    return (super.add(element));
+  }
+  public void add (int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException("No nulls allowed");
+    }
+    super.add(element);
+  }
   public NoNullArrayList(int initialCapacity){
     super(initialCapacity);
   }
