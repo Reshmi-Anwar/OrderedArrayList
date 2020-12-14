2ad81eb2a123167a4c435d69005538ea0e910aeb
Reshmi-Anwar <ranwar10@stuy.edu>
2020-12-14 09:31:11 -0500

    fixed 1 error based on tester posted

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -19,7 +19,7 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     if (element == null){
       throw new IllegalArgumentException("No nulls allowed");
     }
-    super.add(element);
+    super.add(index, element);
   }
   public NoNullArrayList(int initialCapacity){
     super(initialCapacity);
